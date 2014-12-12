56c40caf8a3f850d707d75c5c9f98c373f6cd67b
Brittany <brit.cole@hotmail.com>
2014-12-12 11:59:10 -0400

    TPexp startup

---
+++ TPexp Dec 2014.R
@@ -0,0 +1,4 @@
+setwd("/Users/mac/Google Drive/R Datasets/TP 2013") #home computer
+#note need to change user if working on diff computer
+setwd("/Users/brittany/Google Drive/R Datasets/TP 2013") #lab computer
+TP  <- read.csv("")
